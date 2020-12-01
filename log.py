3b1a1063e744388fb3bf29f0600572ff7d8a8502
Trần Tuấn Anh <trananhkma@gmail.com>
2020-12-01 10:35:48 +0700

    Update alias.cmd

---
+++ log.py
@@ -0,0 +1,27 @@
+import logging
+import sys
+
+from config import LOG_FILE
+
+
+FORMATTER = logging.Formatter("%(asctime)s — %(name)s — %(levelname)s — %(message)s")
+
+
+def get_console_handler():
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(FORMATTER)
+    return console_handler
+
+
+def get_file_handler():
+    file_handler = logging.FileHandler(LOG_FILE, encoding='utf-8')
+    file_handler.setFormatter(FORMATTER)
+    return file_handler
+
+
+def get_logger(logger_name):
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(get_console_handler())
+    logger.addHandler(get_file_handler())
+    return logger
